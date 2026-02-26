54f4af3b5410c0be6fd2c29f8fe8370e440cdcd7
Ahak <ahmadally661@gmail.com>
2026-02-26 12:33:10 +0300

    fith

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 set -o errexit
 
+# Install/upgrade setuptools first to ensure pkg_resources is available
+pip install --upgrade setuptools
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput 
