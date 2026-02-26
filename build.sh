edb3e417f201f3a6e09353e0ce92407adf8da10f
Ahak <ahmadally661@gmail.com>
2026-02-26 14:37:02 +0300

    superuser3

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD 
+    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL 
 fi
